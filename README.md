1a8b5f733b805f538ebeefb119b74b21abb6d280
mike6321 <mike6321@naver.com>
2022-08-22 12:30:29 +0900

    [junwoo.choi] (spring-batch) feat : 프로젝트 구성 및 의존성 설정

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-batch
+
 Spring Batch 스터디를 위한 repository 입니다.
---
+++ src/test/java/com/choi/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choi.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
