0bc4d739b6fb32a115177dad5736a1d4160daed0
mike6321 <mike6321@naver.com>
2022-09-05 23:03:10 +0900

    [junwoo.choi] (spring-batch) feat : JobParameter

--- src/main/java/com/choi/springbatch/JobInstanceConfiguration.java
+++ src/main/java/com/choi/springbatch/JobInstanceConfiguration.java
@@ -2,13 +2,15 @@ package com.choi.springbatch;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -27,7 +29,15 @@ public class JobInstanceConfiguration {
 
     private Step step01() {
         return stepBuilderFactory.get("step01")
-                .tasklet((contribution, chunkContext) -> RepeatStatus.FINISHED)
+                .tasklet((contribution, chunkContext) -> {
+                    JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
+                    jobParameters.getString("name");
+                    jobParameters.getLong("seq");
+                    jobParameters.getDate("date");
+                    jobParameters.getDate("age");
+                    Map<String, Object> parameters = chunkContext.getStepContext().getJobParameters();
+                    return RepeatStatus.FINISHED;
+                })
                 .build();
     }
 
